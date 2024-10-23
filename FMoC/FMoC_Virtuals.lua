d6597f4da7f86813855a18017b5b4eaef9865f5d
TheTrueIndolent <panoulis26@gmail.com>
2024-10-23 14:02:24 +0300

    Update FMoC_Virtuals.lua

--- FMoC/FMoC_Virtuals.lua
+++ FMoC/FMoC_Virtuals.lua
@@ -1,44 +1,48 @@
--- function for showing the menu --
-function fmocShowMenu()
-	if UnitLevel("player") >= 10 and GetSpecialization() ~= 5 then
-		if not fmocOptions00:IsShown() then
-			fmocOptions00:Show()
-		else
-			fmocOptions00:Hide()
-		end
-	else
-		local fmocTime = GameTime_GetTime(false)
-		DEFAULT_CHAT_FRAME:AddMessage(fmocTime.." |A:"..C_AddOns.GetAddOnMetadata("FMoC", "IconAtlas")..":16:16|a ["..fmocMainColor:WrapTextInColorCode(C_AddOns.GetAddOnMetadata("FMoC", "Title")).."] You have to be level 10 in order to use this add on!")
-	end
-end
--- Slash Command --
-SLASH_FUNKYMOJOCHAGER1, SLASH_FUNKYMOJOCHAGER2 = '/fmoc', '/funkymojochanger'
-function SlashCmdList.FUNKYMOJOCHAGER(msg, editBox)
-	fmocShowMenu()
-end
 -- Colors --
 fmocMainColor = CreateColorFromRGBAHexString("F0E68CFF")
 fmocHighColor = CreateColorFromRGBAHexString("FF7F50FF")
 fmocNoMainColor = CreateColorFromRGBAHexString("F0E68C00")
 fmocNoHighColor = CreateColorFromRGBAHexString("FF7F5000")
--- Mini Map Button Functions --
--- Clicky Clicky --
-function fmocMinimapClick(addonName, buttonName)
-	if buttonName == "LeftButton" then
-		fmocShowMenu()
+-- function for showing the menu --
+function fmocShowMenu()
+	if not InCombatLockdown() then
+		if UnitLevel("player") >= 10 and GetSpecialization() ~= 5 then
+			if not fmocOptions00:IsShown() then
+				fmocOptions00:Show()
+			else
+				fmocOptions00:Hide()
+			end
+		else
+			local fmocTime = GameTime_GetTime(false)
+			DEFAULT_CHAT_FRAME:AddMessage(fmocTime.." |A:"..C_AddOns.GetAddOnMetadata("FMoC", "IconAtlas")..":16:16|a ["..fmocMainColor:WrapTextInColorCode(C_AddOns.GetAddOnMetadata("FMoC", "Title")).."] You have to be level 10 in order to use this add on!")
+		end
+	else
+		local fmocTime = GameTime_GetTime(false)
+		DEFAULT_CHAT_FRAME:AddMessage(fmocTime.." |A:"..C_AddOns.GetAddOnMetadata("FMoC", "IconAtlas")..":16:16|a ["..fmocMainColor:WrapTextInColorCode(C_AddOns.GetAddOnMetadata("FMoC", "Title")).."] While you are in combat, you can't do this!")
 	end
 end
--- On Enter --
-function fmocMinimapOnEnter()
-	GameTooltip_ClearStatusBars(GameTooltip)
-	GameTooltip_SetDefaultAnchor(GameTooltip, UIParent)
-	GameTooltip:SetText("|A:"..C_AddOns.GetAddOnMetadata("FMoC", "IconAtlas")..":16:16|a "..fmocMainColor:WrapTextInColorCode(C_AddOns.GetAddOnMetadata("FMoC", "Title")).."|nClick: "..fmocMainColor:WrapTextInColorCode("Open the main panel of settings!")) 
-	GameTooltip:Show()
-end
--- On Leave --
-function fmocMinimapOnLeave()
-	GameTooltip:Hide()
-end
+-- Slash Command --
+RegisterNewSlashCommand(fmocShowMenu, "fmoc", "funkymojochanger")
+-- Mini Map Button Functions --
+AddonCompartmentFrame:RegisterAddon({
+	text = fmocMainColor:WrapTextInColorCode(C_AddOns.GetAddOnMetadata("FMoC", "Title")),
+	icon = C_AddOns.GetAddOnMetadata("FMoC", "IconAtlas"),
+	notCheckable = true,
+	func = function(button, menuInputData, menu)
+		local buttonName = menuInputData.buttonName
+		if buttonName == "LeftButton" then
+			fmocShowMenu()
+		end
+	end,
+	funcOnEnter = function(button)
+		MenuUtil.ShowTooltip(button, function(tooltip)
+		tooltip:SetText("|A:"..C_AddOns.GetAddOnMetadata("FMoC", "IconAtlas")..":16:16|a "..fmocMainColor:WrapTextInColorCode(C_AddOns.GetAddOnMetadata("FMoC", "Title")).."|nLeft Click: "..fmocMainColor:WrapTextInColorCode("Open the main panel of settings!"))
+		end)
+	end,
+	funcOnLeave = function(button)
+		MenuUtil.HideTooltip(button)
+	end,
+})
 -- functions for the buttons and popouts --
 -- on enter --
 function fmocEnteringMenus(self)
